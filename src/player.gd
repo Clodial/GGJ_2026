1f679dcd90966b468a4f4b395097461758993dd7
Clodial <maravillamatthew@gmail.com>
2026-02-08 20:07:36 -0500

    updated tank controls

--- src/player.gd
+++ src/player.gd
@@ -1,12 +1,16 @@
 extends CharacterBody3D
 
 @export var speed = 4
+@export var backSpeed = 2
+@export var turnSpeed = 0.025 
 @export var basic_mask: PackedScene
 @export var basic_mask_label = "basic"
 var is_displaying_basic_mask = false
 @export var web_mask: PackedScene
 @export var web_mask_label = "web"
 var is_displaying_web_mask = false
+@export var is_tank_controls = true
+
 
 var target_velocity = Vector3.ZERO
 var masks = []
@@ -31,27 +35,70 @@ func _physics_process(delta: float) -> void:
 	var horizontal_direction = Input.get_axis("move_left", "move_right")
 	var vertical_direction = Input.get_axis("move_up", "move_down")
 	
-	if horizontal_direction:
-		velocity.x = speed * horizontal_direction
-	
-	#basic input movement
-	if Input.is_action_pressed("move_right"):
-		direction.x += 1
-	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("move_up"):
-		direction.z -= 1
-	if Input.is_action_pressed("move_down"):
-		direction.z += 1
+	#basic tank input movement
+	if is_tank_controls:
+		if Input.is_action_pressed("move_forward") and Input.is_action_pressed("move_back"):
+			velocity.x = 0
+			velocity.z = 0
 
-	#direction movement
-	if direction != Vector3.ZERO:
-		direction = direction.normalized()
-		$pivot.basis = Basis.looking_at(direction)
+		elif Input.is_action_pressed("move_up"):
+			var forwardVector = -Vector3.FORWARD.rotated(Vector3.UP, rotation.y)
+			velocity = -forwardVector * speed
+			
+		elif Input.is_action_pressed("move_down"):
+			var backwardVector = Vector3.FORWARD.rotated(Vector3.UP, rotation.y)
+			velocity = -backwardVector * backSpeed
+		
+		#If pressing nothing stop velocity
+		else:
+			velocity.x = 0
+			velocity.z = 0
+		
+		# IF turn left WHILE moving back, turn right
+		if Input.is_action_pressed("move_left") and Input.is_action_pressed("move_back"):
+			rotation.z -= direction.y + turnSpeed
+			rotation.z = clamp(rotation.x, -50, 90)
+			rotation.y -= direction.y + turnSpeed
+		
+		elif Input.is_action_pressed("move_left"):
+			rotation.z += direction.y - turnSpeed
+			rotation.z = clamp(rotation.x, -50, 90)
+			rotation.y += direction.y + turnSpeed
 
-	#ground velocity
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
+		# IF turn right WHILE moving back, turn left
+		if Input.is_action_pressed("move_right") and Input.is_action_pressed("move_back"):
+			rotation.z += direction.y - turnSpeed
+			rotation.z = clamp(rotation.x, -50, 90)
+			rotation.y += direction.y + turnSpeed
+			
+		elif Input.is_action_pressed("move_right"):
+			rotation.z -= direction.y + turnSpeed
+			rotation.z = clamp(rotation.x, -50, 90)
+			rotation.y -= direction.y + turnSpeed
+		
+	else:
+		if horizontal_direction:
+			velocity.x = speed * horizontal_direction
+		
+		if Input.is_action_pressed("move_right"):
+			direction.x += 1
+		if Input.is_action_pressed("move_left"):
+			direction.x -= 1
+		if Input.is_action_pressed("move_up"):
+			direction.z -= 1
+		if Input.is_action_pressed("move_down"):
+			direction.z += 1
+
+		#direction movement
+		if direction != Vector3.ZERO:
+			direction = direction.normalized()
+			$pivot.basis = Basis.looking_at(direction)
+
+		#ground velocity
+		target_velocity.x = direction.x * speed
+		target_velocity.z = direction.z * speed
+		
+		velocity = target_velocity
 
 	#Collision checking based on top of enemy
 	for index in range(get_slide_collision_count()):
@@ -59,7 +106,6 @@ func _physics_process(delta: float) -> void:
 		if collision.get_collider() == null:
 			continue
 			
-	velocity = target_velocity
 	move_and_slide()
 	
 	if direction != Vector3.ZERO:
